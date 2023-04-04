43b9f8b66e57f17f70beb88afbaa523e2a359717
Opeyemi Michael <the.opeyemimichael@gmail.com>
2023-04-05 00:55:34 +0100

    modified:   budget_app/budget_manager/__pycache__/admin.cpython-311.pyc 	modified:   budget_app/budget_manager/admin.py 	modified:   budget_app/db.sqlite3

--- budget_app/budget_manager/admin.py
+++ budget_app/budget_manager/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Transaction
 
-# Register your models here.
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('date', 'description', 'amount')
+
+admin.site.register(Transaction, TransactionAdmin)
