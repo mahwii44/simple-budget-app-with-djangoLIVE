9819b7b916271b7e611b7d6b451d07c2af965c7d
Opeyemi Michael <the.opeyemimichael@gmail.com>
2023-04-06 00:15:03 +0100

    modified:   budget_app/budget_manager/views.py

--- budget_app/budget_manager/views.py
+++ budget_app/budget_manager/views.py
@@ -17,15 +17,21 @@ def transaction_create(request):
         form = TransactionForm()
     return render(request, 'newbudget.html', {'form': form})
 
+def transaction_update(request, pk):
+    transaction = get_object_or_404(Transaction, pk=pk)
+    if request.method == 'POST':
+        form = TransactionForm(request.POST, instance=transaction)
+        if form.is_valid():
+            form.save()
+            return redirect('transaction_list')
+    else:
+        form = TransactionForm(instance=transaction)
+    return render(request, 'update.html', {'form': form})
 
+def transaction_delete(request, pk):
+    transaction = get_object_or_404(Transaction, pk=pk)
+    if request.method == 'POST':
+        transaction.delete()
+        return redirect('transaction_list')
+    return render(request, 'delete.html', {'transaction': transaction})
 
-class TransactionUpdateView(UpdateView):
-    model = Transaction
-    template_name = 'index.html'
-    fields = ['date', 'description', 'amount']
-    success_url = reverse_lazy('transaction')
-
-class TransactionDeleteView(DeleteView):
-    model = Transaction
-    template_name = 'delete.html'
-    success_url = reverse_lazy('transaction')
