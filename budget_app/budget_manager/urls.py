2307a240c6d9d0941b66babd004fb61082b41fb7
Opeyemi Michael <the.opeyemimichael@gmail.com>
2023-04-06 00:44:29 +0100

    modified:   budget_app/budget_manager/urls.py

--- budget_app/budget_manager/urls.py
+++ budget_app/budget_manager/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.transaction_list, name='transaction_list'),
-    path('new/', views.transaction_create, name='transaction_create'),
+    path('newbudget/', views.transaction_create, name='transaction_create'),
     path('<int:pk>/update/', views.transaction_update, name='transaction_update'),
     path('<int:pk>/delete/', views.transaction_delete, name='transaction_delete'),
 ]
